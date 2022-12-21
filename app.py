491fe1ea97334046992b16de3951dd825f127799
raju-komati <114348142+raju-komati@users.noreply.github.com>
2022-12-21 23:50:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,19 @@ import streamlit as st
 import requests
 import pandas as pd
 
-@st.cache(ttl=60*60*12, allow_output_mutation=True)
-def fetch_emojis():
-    resp = requests.get(
-        'https://raw.githubusercontent.com/omnidan/node-emoji/master/lib/emoji.json')
-    json = resp.json()
-    codes, emojis = zip(*json.items())
-    return pd.DataFrame({
-        'Emojis': emojis,
-        'Shortcodes': [f':{code}:' for code in codes],
-    })
+
+resp = requests.get(
+    'https://raw.githubusercontent.com/omnidan/node-emoji/master/lib/emoji.json')
+json = resp.json()
+codes, emojis = zip(*json.items())
+df_emoji = pd.DataFrame({
+    'Emojis': emojis,
+    'Shortcodes': [f':{code}:' for code in codes],
+})
+
+emoji1 = st.select_slider('Select emoji 1:', options = df_emoji['shortcodes'], value=':sunglasses:')
+emoji2 = st.select_slider('Select emoji 2:', options = df_emoji['shortcodes'], value=':white_check_mark:')
+emoji3 = st.select_slider('Select emoji 3:', options = df_emoji['shortcodes'], value=':coffee:')
     
-emojis = fetch_emojis()
 
 st.table(emojis)
